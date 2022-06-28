11320fdd4da666f222af9a523df2b2192feafd43
Jefferson Monteiro <jeffmonteiro61@icloud.com>
2022-06-28 10:02:18 -0300

    chore:interactionAlertConfirm

--- src/main/java/TesteAlert.java
+++ src/main/java/TesteAlert.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
+
 public class TesteAlert {
 	
 	@Test
@@ -25,5 +26,29 @@ public class TesteAlert {
 		alert.accept();
 		
 		driver.findElement(By.id("elementosForm:nome")).sendKeys(texto);
+	}
+
+	@Test
+	public void deveInteragirComConfirm(){
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().setSize(new Dimension(1200, 765));
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+
+		driver.findElement(By.id("confirm")).click();
+		Alert confirm = driver.switchTo().alert();
+		Assert.assertEquals("Confirm Simples", confirm.getText());
+		confirm.accept();
+		Assert.assertEquals("Confirmado", confirm.getText());
+		confirm.accept();
+
+		driver.findElement(By.id("confirm")).click();
+		confirm = driver.switchTo().alert();
+		Assert.assertEquals("Confirm Simples", confirm.getText());
+		confirm.dismiss();
+		String text = confirm.getText();
+		Assert.assertEquals("Negado", text);
+		confirm.accept();
+
+		driver.quit();
 	}
 }
