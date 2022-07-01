f69bf92fb619ca5679e1dd691bada9449950f70e
Jefferson Monteiro <jeffmonteiro61@icloud.com>
2022-06-30 22:22:44 -0300

    secondChallenge

--- src/main/java/challenges/SecondChallenge.java
+++ src/main/java/challenges/SecondChallenge.java
@@ -1,11 +1,24 @@
 package challenges;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class SecondChallenge {
 
     @Test
     public void testarCampoName(){
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().window().setSize(new Dimension(1200, 765));
+        driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
 
+        driver.findElement(By.id("elementosForm:cadastrar")).click();
+        Alert alert = driver.switchTo().alert();
+        Assert.assertEquals("Nome eh obrigatorio", alert.getText());
+        driver.quit();
     }
 }
