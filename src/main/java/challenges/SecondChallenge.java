ec044fb9eab9a775090fa8904405797af058b65b
Jefferson <jefferson.moura@hp.com>
2022-07-03 16:01:49 -0300

    secondChallenge

--- src/main/java/challenges/SecondChallenge.java
+++ src/main/java/challenges/SecondChallenge.java
@@ -11,14 +11,29 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class SecondChallenge {
 
     @Test
-    public void testarCampoName(){
+    public void validarCampoName(){
         WebDriver driver = new FirefoxDriver();
         driver.manage().window().setSize(new Dimension(1200, 765));
         driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
 
+        driver.findElement(By.id("elementosForm:nome")).sendKeys("Jefferson");
         driver.findElement(By.id("elementosForm:cadastrar")).click();
         Alert alert = driver.switchTo().alert();
         Assert.assertEquals("Nome eh obrigatorio", alert.getText());
         driver.quit();
+    }
+
+    @Test
+    public void validarCampoSobrenome(){
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().window().setSize(new Dimension(1200, 765));
+        driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+
+        driver.findElement(By.id("elementosForm:nome")).sendKeys("Jefferson");
+        driver.findElement(By.id("elementosForm:sobrenome")).sendKeys("Monteiro");
+        driver.findElement(By.id("elementosForm:cadastrar")).click();
+        Alert alert = driver.switchTo().alert();
+        Assert.assertEquals("Sexo eh obrigatorio", alert.getText());
+
     }
 }
