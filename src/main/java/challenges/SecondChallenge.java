358c0ed08864bfeaafb0081fe3edafe523e171dd
Jefferson Monteiro <jeffmonteiro61@icloud.com>
2022-07-07 22:13:49 -0300

    mustSelectSport

--- src/main/java/challenges/SecondChallenge.java
+++ src/main/java/challenges/SecondChallenge.java
@@ -2,11 +2,9 @@ package challenges;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class SecondChallenge {
 
@@ -60,8 +58,32 @@ public class SecondChallenge {
         driver.findElement(By.id("elementosForm:sexo:0")).click();
 
         driver.findElement(By.id("elementosForm:comidaFavorita:0")).click();
-        driver.findElement(By.id("elementosForm:comidaFavorita:3")).click();
+        //driver.findElement(By.id("elementosForm:comidaFavorita:3")).click();
+        driver.findElement(By.id("elementosForm:cadastrar")).click();
+        Alert alert = driver.switchTo().alert();
+        Assert.assertEquals("Tem certeza que voce eh vegetariano?", alert.getText());
+        driver.quit();
+    }
+
+    @Test
+    public void deveSelecionarEsporte(){
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().window().setSize(new Dimension(1200, 765));
+        driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+
+        driver.findElement(By.id("elementosForm:nome")).sendKeys("Jefferson");
+        driver.findElement(By.id("elementosForm:sobrenome")).sendKeys("Monteiro");
+        driver.findElement(By.id("elementosForm:sexo:0")).click();
+        driver.findElement(By.id("elementosForm:comidaFavorita:0")).click();
+        Select combo = new Select(driver.findElement(By.id("elementosForm:esportes")));
+        combo.selectByVisibleText("Karate");
+        combo.selectByVisibleText("O que eh esporte?");
         driver.findElement(By.id("elementosForm:cadastrar")).click();
         driver.quit();
+
+
+
+
+
     }
 }
