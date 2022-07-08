e7e4b1b3f5bd55d9e2f809ab4fcc5e0da26df584
Jefferson Monteiro <jeffmonteiro61@icloud.com>
2022-07-07 23:13:47 -0300

    testAlert

--- src/main/java/TesteAlert.java
+++ src/main/java/TesteAlert.java
@@ -13,13 +13,18 @@ import org.openqa.selenium.support.ui.Select;
 
 
 public class TesteAlert {
+
+	private WebDriver driver;
+
+	public void inicializa(){
+		driver = new FirefoxDriver();
+		driver.manage().window().setSize(new Dimension(1200, 765));
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+	}
 	
 	@Test
 	public void deveInteragirComAlertSimples() {
-		WebDriver driver = new FirefoxDriver();
-		driver.manage().window().setSize(new Dimension(1200, 765));
-		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
-		
+		inicializa();
 		driver.findElement(By.id("alert")).click();
 		Alert alert = driver.switchTo().alert();
 		String texto = alert.getText();
@@ -27,13 +32,12 @@ public class TesteAlert {
 		alert.accept();
 		
 		driver.findElement(By.id("elementosForm:nome")).sendKeys(texto);
+		driver.quit();
 	}
 
 	@Test
 	public void deveInteragirComConfirm(){
-		WebDriver driver = new FirefoxDriver();
-		driver.manage().window().setSize(new Dimension(1200, 765));
-		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		inicializa();
 
 		driver.findElement(By.id("confirm")).click();
 		Alert confirm = driver.switchTo().alert();
@@ -56,9 +60,7 @@ public class TesteAlert {
 	@Test
 	@Ignore
 	public void deveInteragirComPrompt(){
-		WebDriver driver = new FirefoxDriver();
-		driver.manage().window().setSize(new Dimension(1200, 765));
-		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		inicializa();
 
 		driver.findElement(By.id("prompt")).click();
 		Alert alerta = driver.switchTo().alert();
