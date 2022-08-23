558842cb4b593956e0a167fe26bbf6378d9f15f7
Jefferson <jefferson.moura@hp.com>
2022-08-22 21:21:05 -0300

    challenge

--- src/main/java/challenges/challengeTest.java
+++ src/main/java/challenges/challengeTest.java
@@ -19,13 +19,12 @@ public class challengeTest {
         }
 
         @Test
-        public void testarLogin(){
+        public void testLogin(){
             WebDriver driver = new ChromeDriver();
             driver.manage().window().setSize(new Dimension(1200, 765));
             driver.get("http://www.way2automation.com/angularjs-protractor/banking/#/login");
 
-            //driver.findElement(By.xpath("//button[.='Bank Manager Login']")).click();
-
+            driver.findElement(By.xpath("//div[@class='meshim_widget_widgets_IconFont icon_font close']"));
             WebElement clickButton = driver.findElement(By.xpath("//html//body//div[3]//div//div[2]//div//div[1]//div[1]//button"));
             clickButton.click();
 
