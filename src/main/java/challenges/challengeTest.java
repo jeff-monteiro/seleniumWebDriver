d4ee72619476e4c0632d7b56f21a77ea0cb23bac
Jefferson Monteiro <jeffmonteiro61@icloud.com>
2022-08-26 07:56:51 -0300

    fix

--- src/main/java/challenges/challengeTest.java
+++ src/main/java/challenges/challengeTest.java
@@ -1,34 +1,19 @@
 package challenges;
 
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.devtools.v95.css.CSS;
 
 public class challengeTest {
 
         @Test
-        public void testarApplication() {
+        public void testApplication() {
             WebDriver driver = new ChromeDriver();
             driver.manage().window().setSize(new Dimension(1200, 765));
             driver.get("http://www.way2automation.com/angularjs-protractor/banking/#/login");
-            //driver.quit();
-        }
-
-        @Test
-        public void testLogin(){
-            WebDriver driver = new ChromeDriver();
-            driver.manage().window().setSize(new Dimension(1200, 765));
-            driver.get("http://www.way2automation.com/angularjs-protractor/banking/#/login");
-
-            driver.findElement(By.xpath("//div[@class='meshim_widget_widgets_IconFont icon_font close']"));
-            WebElement clickButton = driver.findElement(By.xpath("//html//body//div[3]//div//div[2]//div//div[1]//div[1]//button"));
-            clickButton.click();
-
-
+            Alert alert = driver.switchTo().alert();
+            driver.quit();
         }
 
 }
